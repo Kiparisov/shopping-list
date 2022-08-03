a8cdd2be767c2cb2cc144038d8d4196508547581
Kiparisov <66064323+Kiparisov@users.noreply.github.com>
2022-08-03 22:57:24 +0600

    Revert "Domain layer"

--- app/src/main/java/com/kiparisov/shoppinglist/domain/AddShopItemUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.kiparisov.shoppinglist.domain
-
-class AddShopItemUseCase(private val repository: ShopListRepository) {
-    fun addShopItem(shopItem: ShopItem){
-        repository.addShopItem(shopItem)
-    }
-}
--- app/src/main/java/com/kiparisov/shoppinglist/domain/GetShopItemUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.kiparisov.shoppinglist.domain
-
-class GetShopItemUseCase(private val repository: ShopListRepository) {
-    fun getShopItem(id: Int): ShopItem{
-        return repository.getShopItem(id)
-    }
-}
