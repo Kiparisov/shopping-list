cbbf5964576246ba8ccd32deb377dede8c1c9eb0
Kiparisov <bobbycrasby@yandex.ru>
2022-08-03 22:57:14 +0600

    edit use-case edited

--- app/src/main/java/com/kiparisov/shoppinglist/domain/EditShopItemUseCase.kt
+++ app/src/main/java/com/kiparisov/shoppinglist/domain/EditShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.kiparisov.shoppinglist.domain
 
 class EditShopItemUseCase(private val repository: ShopListRepository) {
-    fun editShopItem(shopItem: ShopItem): ShopItem{
-        return repository.editShopItem(shopItem)
+    fun editShopItem(shopItem: ShopItem){
+        repository.editShopItem(shopItem)
     }
 }
